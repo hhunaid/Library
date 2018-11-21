74d4e66903d5810c9f36a7f29fff7775e32e02a2
Hunaid Hassan <hhunaid@gmail.com>
2018-11-21 19:43:04 +0500

    Initial Commit

---
+++ LibraryData/Migrations/20181121141109_initialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryData.Migrations
+{
+    public partial class initialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patrons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    TelophoneNumber = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patrons", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Patrons");
+        }
+    }
+}
---
+++ LibraryData/Models/Patron.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace LibraryData.Models
+{
+    public class Patron
+    {
+        public int Id { get; set; }
+        public string FirstName {get; set;} 
+        public string LastName {get; set;} 
+        public string Address {get; set;} 
+        public string TelophoneNumber {get; set;}
+        public DateTime DateOfBirth { get; set; }
+
+        // public virtual LibraryCard LibraryCard { get; set; }
+    }
+}
